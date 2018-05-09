714179ab9e5c2671eb2ae705fad2c937e349d6d4
Lai Tang <laitang2000@yahoo.com>
2018-05-09 23:16:25 +0100

    ci
    and again!

--- integration/EmailTests.cs
+++ integration/EmailTests.cs
@@ -27,6 +27,7 @@ namespace integration
                 RequestUri = new Uri($"{GeneratorApiRoot}/EmailRandomNames")
             };
             Console.WriteLine($"Sending email: {sendEmail.RequestUri}");
+            Console.WriteLine($"Sending email: {sendEmail.RequestUri}");
             using (var response = await client.SendAsync(sendEmail))
             {
                 response.EnsureSuccessStatusCode();
